f6adda8f661b1b6f07c29ad0266e97dd0351c293
glang <glang@vm-0.glang.koding.kd.io>
2014-03-26 07:21:32 +0000

    working WordPress installer

---
+++ index.coffee
@@ -0,0 +1,233 @@
+class LogWatcher extends FSWatcher
+
+  fileAdded:(change)->
+    {name} = change.file
+    [percentage, status] = name.split '-'
+    
+    @emit "UpdateProgress", percentage, status
+
+domain     = "#{KD.nick()}.kd.io"
+OutPath    = "/tmp/_WordPressinstaller.out"
+wordpressIndex    = "~/Web/wordpress/index.php"
+png = "https://raw.githubusercontent.com/glang/wordpress.kdapp/master/wordpress.png"
+
+ 
+class WordpressMainView extends KDView 
+
+  constructor:->
+    super cssClass: "WordPress-installer" 
+
+  viewAppended:->
+
+    KD.singletons.appManager.require 'Terminal', =>
+
+      @addSubView @header = new KDHeaderView
+        title         : "WordPress Installer"
+        type          : "big"
+
+      @addSubView @toggle = new KDToggleButton
+        cssClass        : 'toggle-button'
+        style           : "clean-gray" 
+        defaultState    : "Show details"
+        states          : [
+          title         : "Show details"
+          callback      : (cb)=>
+            @terminal.setClass 'in'
+            @toggle.setClass 'toggle'
+            @terminal.webterm.setKeyView()
+            cb?()
+        ,
+          title         : "Hide details"
+          callback      : (cb)=>
+            @terminal.unsetClass 'in'
+            @toggle.unsetClass 'toggle'
+            cb?()
+        ]
+
+      @addSubView @logo = new KDCustomHTMLView
+        tagName       : 'img'
+        cssClass      : 'logo'
+        attributes    :
+          src         : png
+
+      @watcher = new LogWatcher
+
+      @addSubView @progress = new KDProgressBarView
+        initial       : 100
+        title         : "Checking installation..."
+
+      @addSubView @terminal = new TerminalPane
+        cssClass      : 'terminal'
+
+      @addSubView @button = new KDButtonView
+        title         : "Install WordPress"
+        cssClass      : 'main-button solid'
+        loader        :
+          color       : "#FFFFFF"
+          diameter    : 12
+        callback      : => @installCallback()
+
+      @addSubView @link = new KDCustomHTMLView
+        cssClass : 'hidden running-link'
+        
+      @link.setSession = (session)->
+        @updatePartial "Click here to launch WordPress: <a target='_blank' href='http://#{domain}/wordpress/index.php'>http://#{domain}/wordpress/index.php</a>"
+        @show()
+
+      @addSubView @content = new KDCustomHTMLView
+        cssClass : "WordPress-help"
+        partial  : """
+          <p>This is an early version of WordPress, a free open-source blogging tool and a content
+          management system based on PHP and MySQL which runs on a web hosting service. </p>
+          
+          <p>Why should you use WordPress?</p>
+          
+          <ul>
+            <li>
+            <strong>WordPress is Free and Open Source</strong> Unlike other "free" solutions, WordPress is completely free forever.
+            </li>
+            <li>
+            <strong>Plugins That Give You More Power</strong> Plugins allow you to add photo galleries, sliders, shopping carts,
+             forums, maps, and more great functionality.  WordPress includes a searchable, one-click install directory of plugins
+             (like an App Store for WordPress). </li>
+            <li>
+            <strong>Intuitive User-Friendly Backend .</strong>
+            </li>
+            <li>
+            <strong>Themes Allow You To Style Your Site .</strong>
+            </li>
+            <li>
+            <strong>Easy to Update and Keep Secure .</strong>
+            </li>
+            <li>
+            <strong>WordPress Sites are Simple and Accessible .</strong>
+            </li>
+            <li>
+            <strong>Your Sites Can Grow With You.</strong> You can easily upgrade your site with new features and security. 
+            New themes, plugins, and other features can be added without redoing the entire site. 
+            </li>
+
+          </ul>
+          
+          <p>You can see some <a href="http://wordpress.org/showcase/">examples </a> of sites that have used WordPress among which 
+          include The New York Times Blog, TechCrunch, Flickr, and many others.  <a href="https://codex.wordpress.org/WordPress_Lessons">online tutorials</a>,
+           and news on the <a href="https://wordpress.org/news/">WordPress blog</a>.</p>
+        """
+
+      @checkState()
+
+  checkState:->
+
+    vmc = KD.getSingleton 'vmController'
+
+    @button.showLoader()
+
+    FSHelper.exists wordpressIndex, vmc.defaultVmName, (err, WordPress)=>
+      warn err if err
+      
+      unless WordPress
+        @link.hide()
+        @progress.updateBar 100, '%', "WordPress is not installed."
+        @switchState 'install'
+      else
+        @progress.updateBar 100, '%', "WordPress is installed."
+        @link.setSession "Web/wordpress/index.php"
+        @switchState 'run'
+
+  
+  switchState:(state = 'run')->
+
+    @watcher.off 'UpdateProgress'
+
+    switch state
+      when 'install'
+        title = "Install WordPress"
+        style = ''
+        @button.setCallback => @installCallback()
+      when 'run'
+        @button.hide()
+        ###
+        title = "Running Wordpress"
+        style = ''
+        @button.setCallback => @stopCallback()
+        ###
+    @button.unsetClass 'red green'
+    @button.setClass style
+    @button.setTitle title or "Run WordPress"
+    @button.hideLoader()
+
+  stopCallback:->
+    @_lastRequest = 'stop'
+    # @terminal.runCommand "pkill -f #{wordpressIndex} -u #{KD.nick()}"
+    ###KD.utils.wait 3000, => ###
+    @button.hide()
+    @checkState()
+
+  runCallback:->
+    @_lastRequest = 'run'
+    session = (Math.random() + 1).toString(36).substring 7
+    # @terminal.runCommand "node #{kdbPath}/WordPress.js #{session} &"
+    ###KD.utils.wait 1000, => ###
+    @checkState()
+
+  installCallback:->
+    @watcher.on 'UpdateProgress', (percentage, status)=>
+      @progress.updateBar percentage, '%', status
+      if percentage is "100"
+        @button.hideLoader()
+        @toggle.setState 'Show details'
+        @terminal.unsetClass 'in'
+        @toggle.unsetClass 'toggle'
+        #test
+        @link.setSession "Web/wordpress/index.php"
+        @switchState 'run'
+        # @switchState 'run'
+      else if percentage is "0"
+        @toggle.setState 'Hide details'
+        @terminal.setClass 'in'
+        @toggle.setClass 'toggle'
+        @terminal.webterm.setKeyView()
+
+    session = (Math.random() + 1).toString(36).substring 7
+    tmpOutPath = "#{OutPath}/#{session}"
+    vmc = KD.getSingleton 'vmController'
+    vmc.run "rm -rf #{OutPath}; mkdir -p #{tmpOutPath}", =>
+      @watcher.stopWatching()
+      @watcher.path = tmpOutPath
+      @watcher.watch()
+      @terminal.runCommand "curl --silent https://raw.githubusercontent.com/glang/wordpress.kdapp/master/newInstaller.sh | bash -s #{session}"
+
+  isWordpressRunning:(callback)->
+    vmc = KD.getSingleton 'vmController'
+    vmc.run "pgrep -f '.koding-WordPress/WordPress.js' -l -u #{KD.nick()}", (err, res)->
+      callback false
+      # if err then callback false
+      # else callback res.split(' ').last
+
+class WordpressController extends AppController
+
+  constructor:(options = {}, data)->
+    options.view    = new WordpressMainView
+    options.appInfo =
+      name : "Wordpress"
+      type : "application"
+
+    super options, data
+
+do ->
+
+  # In live mode you can add your App view to window's appView
+  if appView?
+
+    view = new WordpressMainView
+    appView.addSubView view
+
+  else
+
+    KD.registerAppClass WordpressController,
+      name     : "Wordpress"
+      routes   :
+        "/:name?/Wordpress" : null
+        "/:name?/glang/Apps/Wordpress" : null
+      dockPath : "/glang/Apps/Wordpress"
+      behavior : "application"
---
+++ index.js
@@ -0,0 +1,264 @@
+/* Compiled by kdc on Wed Mar 26 2014 07:17:33 GMT+0000 (UTC) */
+(function() {
+/* KDAPP STARTS */
+/* BLOCK STARTS: /home/glang/Applications/Wordpress.kdapp/index.coffee */
+var LogWatcher, OutPath, WordpressController, WordpressMainView, domain, png, wordpressIndex, _ref,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+LogWatcher = (function(_super) {
+  __extends(LogWatcher, _super);
+
+  function LogWatcher() {
+    _ref = LogWatcher.__super__.constructor.apply(this, arguments);
+    return _ref;
+  }
+
+  LogWatcher.prototype.fileAdded = function(change) {
+    var name, percentage, status, _ref1;
+    name = change.file.name;
+    _ref1 = name.split('-'), percentage = _ref1[0], status = _ref1[1];
+    return this.emit("UpdateProgress", percentage, status);
+  };
+
+  return LogWatcher;
+
+})(FSWatcher);
+
+domain = "" + (KD.nick()) + ".kd.io";
+
+OutPath = "/tmp/_WordPressinstaller.out";
+
+wordpressIndex = "~/Web/wordpress/index.php";
+
+png = "https://raw.githubusercontent.com/glang/wordpress.kdapp/master/wordpress.png";
+
+WordpressMainView = (function(_super) {
+  __extends(WordpressMainView, _super);
+
+  function WordpressMainView() {
+    WordpressMainView.__super__.constructor.call(this, {
+      cssClass: "WordPress-installer"
+    });
+  }
+
+  WordpressMainView.prototype.viewAppended = function() {
+    var _this = this;
+    return KD.singletons.appManager.require('Terminal', function() {
+      _this.addSubView(_this.header = new KDHeaderView({
+        title: "WordPress Installer",
+        type: "big"
+      }));
+      _this.addSubView(_this.toggle = new KDToggleButton({
+        cssClass: 'toggle-button',
+        style: "clean-gray",
+        defaultState: "Show details",
+        states: [
+          {
+            title: "Show details",
+            callback: function(cb) {
+              _this.terminal.setClass('in');
+              _this.toggle.setClass('toggle');
+              _this.terminal.webterm.setKeyView();
+              return typeof cb === "function" ? cb() : void 0;
+            }
+          }, {
+            title: "Hide details",
+            callback: function(cb) {
+              _this.terminal.unsetClass('in');
+              _this.toggle.unsetClass('toggle');
+              return typeof cb === "function" ? cb() : void 0;
+            }
+          }
+        ]
+      }));
+      _this.addSubView(_this.logo = new KDCustomHTMLView({
+        tagName: 'img',
+        cssClass: 'logo',
+        attributes: {
+          src: png
+        }
+      }));
+      _this.watcher = new LogWatcher;
+      _this.addSubView(_this.progress = new KDProgressBarView({
+        initial: 100,
+        title: "Checking installation..."
+      }));
+      _this.addSubView(_this.terminal = new TerminalPane({
+        cssClass: 'terminal'
+      }));
+      _this.addSubView(_this.button = new KDButtonView({
+        title: "Install WordPress",
+        cssClass: 'main-button solid',
+        loader: {
+          color: "#FFFFFF",
+          diameter: 12
+        },
+        callback: function() {
+          return _this.installCallback();
+        }
+      }));
+      _this.addSubView(_this.link = new KDCustomHTMLView({
+        cssClass: 'hidden running-link'
+      }));
+      _this.link.setSession = function(session) {
+        this.updatePartial("Click here to launch WordPress: <a target='_blank' href='http://" + domain + "/wordpress/index.php'>http://" + domain + "/wordpress/index.php</a>");
+        return this.show();
+      };
+      _this.addSubView(_this.content = new KDCustomHTMLView({
+        cssClass: "WordPress-help",
+        partial: "<p>This is an early version of WordPress, a free open-source blogging tool and a content\nmanagement system based on PHP and MySQL which runs on a web hosting service. </p>\n\n<p>Why should you use WordPress?</p>\n\n<ul>\n  <li>\n  <strong>WordPress is Free and Open Source</strong> Unlike other \"free\" solutions, WordPress is completely free forever.\n  </li>\n  <li>\n  <strong>Plugins That Give You More Power</strong> Plugins allow you to add photo galleries, sliders, shopping carts,\n   forums, maps, and more great functionality.  WordPress includes a searchable, one-click install directory of plugins\n   (like an App Store for WordPress). </li>\n  <li>\n  <strong>Intuitive User-Friendly Backend .</strong>\n  </li>\n  <li>\n  <strong>Themes Allow You To Style Your Site .</strong>\n  </li>\n  <li>\n  <strong>Easy to Update and Keep Secure .</strong>\n  </li>\n  <li>\n  <strong>WordPress Sites are Simple and Accessible .</strong>\n  </li>\n  <li>\n  <strong>Your Sites Can Grow With You.</strong> You can easily upgrade your site with new features and security. \n  New themes, plugins, and other features can be added without redoing the entire site. \n  </li>\n\n</ul>\n\n<p>You can see some <a href=\"http://wordpress.org/showcase/\">examples </a> of sites that have used WordPress among which \ninclude The New York Times Blog, TechCrunch, Flickr, and many others.  <a href=\"https://codex.wordpress.org/WordPress_Lessons\">online tutorials</a>,\n and news on the <a href=\"https://wordpress.org/news/\">WordPress blog</a>.</p>"
+      }));
+      return _this.checkState();
+    });
+  };
+
+  WordpressMainView.prototype.checkState = function() {
+    var vmc,
+      _this = this;
+    vmc = KD.getSingleton('vmController');
+    this.button.showLoader();
+    return FSHelper.exists(wordpressIndex, vmc.defaultVmName, function(err, WordPress) {
+      if (err) {
+        warn(err);
+      }
+      if (!WordPress) {
+        _this.link.hide();
+        _this.progress.updateBar(100, '%', "WordPress is not installed.");
+        return _this.switchState('install');
+      } else {
+        _this.progress.updateBar(100, '%', "WordPress is installed.");
+        _this.link.setSession("Web/wordpress/index.php");
+        return _this.switchState('run');
+      }
+    });
+  };
+
+  WordpressMainView.prototype.switchState = function(state) {
+    var style, title,
+      _this = this;
+    if (state == null) {
+      state = 'run';
+    }
+    this.watcher.off('UpdateProgress');
+    switch (state) {
+      case 'install':
+        title = "Install WordPress";
+        style = '';
+        this.button.setCallback(function() {
+          return _this.installCallback();
+        });
+        break;
+      case 'run':
+        this.button.hide();
+        /*
+        title = "Running Wordpress"
+        style = ''
+        @button.setCallback => @stopCallback()
+        */
+
+    }
+    this.button.unsetClass('red green');
+    this.button.setClass(style);
+    this.button.setTitle(title || "Run WordPress");
+    return this.button.hideLoader();
+  };
+
+  WordpressMainView.prototype.stopCallback = function() {
+    this._lastRequest = 'stop';
+    /*KD.utils.wait 3000, =>*/
+
+    this.button.hide();
+    return this.checkState();
+  };
+
+  WordpressMainView.prototype.runCallback = function() {
+    var session;
+    this._lastRequest = 'run';
+    session = (Math.random() + 1).toString(36).substring(7);
+    /*KD.utils.wait 1000, =>*/
+
+    return this.checkState();
+  };
+
+  WordpressMainView.prototype.installCallback = function() {
+    var session, tmpOutPath, vmc,
+      _this = this;
+    this.watcher.on('UpdateProgress', function(percentage, status) {
+      _this.progress.updateBar(percentage, '%', status);
+      if (percentage === "100") {
+        _this.button.hideLoader();
+        _this.toggle.setState('Show details');
+        _this.terminal.unsetClass('in');
+        _this.toggle.unsetClass('toggle');
+        _this.link.setSession("Web/wordpress/index.php");
+        return _this.switchState('run');
+      } else if (percentage === "0") {
+        _this.toggle.setState('Hide details');
+        _this.terminal.setClass('in');
+        _this.toggle.setClass('toggle');
+        return _this.terminal.webterm.setKeyView();
+      }
+    });
+    session = (Math.random() + 1).toString(36).substring(7);
+    tmpOutPath = "" + OutPath + "/" + session;
+    vmc = KD.getSingleton('vmController');
+    return vmc.run("rm -rf " + OutPath + "; mkdir -p " + tmpOutPath, function() {
+      _this.watcher.stopWatching();
+      _this.watcher.path = tmpOutPath;
+      _this.watcher.watch();
+      return _this.terminal.runCommand("curl --silent https://raw.githubusercontent.com/glang/wordpress.kdapp/master/newInstaller.sh | bash -s " + session);
+    });
+  };
+
+  WordpressMainView.prototype.isWordpressRunning = function(callback) {
+    var vmc;
+    vmc = KD.getSingleton('vmController');
+    return vmc.run("pgrep -f '.koding-WordPress/WordPress.js' -l -u " + (KD.nick()), function(err, res) {
+      return callback(false);
+    });
+  };
+
+  return WordpressMainView;
+
+})(KDView);
+
+WordpressController = (function(_super) {
+  __extends(WordpressController, _super);
+
+  function WordpressController(options, data) {
+    if (options == null) {
+      options = {};
+    }
+    options.view = new WordpressMainView;
+    options.appInfo = {
+      name: "Wordpress",
+      type: "application"
+    };
+    WordpressController.__super__.constructor.call(this, options, data);
+  }
+
+  return WordpressController;
+
+})(AppController);
+
+(function() {
+  var view;
+  if (typeof appView !== "undefined" && appView !== null) {
+    view = new WordpressMainView;
+    return appView.addSubView(view);
+  } else {
+    return KD.registerAppClass(WordpressController, {
+      name: "Wordpress",
+      routes: {
+        "/:name?/Wordpress": null,
+        "/:name?/glang/Apps/Wordpress": null
+      },
+      dockPath: "/glang/Apps/Wordpress",
+      behavior: "application"
+    });
+  }
+})();
+
+/* KDAPP ENDS */
+}).call();
