60fd36b68b5ca6d940aae73feeacaf11b46e8f55
glang <glang@vm-8.glang.koding.kd.io>
2014-07-03 19:54:39 +0000

    fix

--- newInstaller.sh
+++ newInstaller.sh
@@ -46,7 +46,7 @@ echo "If you have NOT changed your MySQL root password, which, by default, is no
 echo
 echo "If you have changed your MySQL root password, please enter your password now."
 echo
-read -ps "Enter password and press enter (you will not see the password): " password
+read -s -p "Enter password and press enter (you will not see the password): " password
 mysql -u root -p$password -e "CREATE DATABASE wordpress_db;"
 
 #create wp config
